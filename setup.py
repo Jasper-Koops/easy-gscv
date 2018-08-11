f34faad27054051de148485d8c97d0e0016054d5
Jasper Koops <jasperkoops91@gmail.com>
2018-08-11 17:12:57 +0200

    Updated version number in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="easy-gscv",
-    version="0.1.2",
+    version="0.2",
     author="Jasper Koops",
     author_email="jasperkoops91@gmail.com",
     description=(
